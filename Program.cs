4e05a31b42d64e3328ad895ca84c25e052e0c51c
davidcba01 <davoocordoba2001@gmail.com>
2025-05-20 15:24:43 -0300

    punto1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Runtime.InteropServices;
+
+
+int c;
+
+Console.WriteLine("Ingrese un numero:");
+string entrada=Console.ReadLine();
+c=int.Parse(entrada); 
+
+while(c>0){
+
+
+Console.Write(c%10);
+c=c/10;
+}
