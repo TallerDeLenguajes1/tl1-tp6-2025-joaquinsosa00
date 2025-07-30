5665dbca4bf4b066721e62c74f846d54f98d7d33
joaquinsosa00 <joaquin150496@gmail.com>
2025-07-29 21:06:19 -0300

    Terminado

--- proyecto1/Program.cs
+++ proyecto1/Program.cs
@@ -1,92 +1,199 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//Ejercicio 1
+using Microsoft.VisualBasic;
 
-//-------------------------------------- Punto 1 --------------------------------------
+Console.WriteLine("Ingrese un Numero:");
+string? s = Console.ReadLine();
+int i =0;
+bool result = int.TryParse(s, out i); //i=numerico
 
-
-
-
-//Console.WriteLine("Ingrese un numero:");
-
-/*string entrada=Console.ReadLine();    //Otra opcion
-numero=int.Parse(entrada); */
-/*
-
-
-int numero2=int.Parse(Console.ReadLine());
-
-while(numero2>0){
-Console.Write(numero2%10);
-numero2=numero2/10;
+if (result)
+{
+    Console.WriteLine("valor ingresado es " + i + " (entero).");
+    if (i<=0)
+    {
+        Console.WriteLine("El valor "+s+" debe ser positivo > 0. ");
+    }else
+    {
+        Console.WriteLine("Numero Invertido:");
+    while (i > 0)
+        {
+            Console.Write(i % 10);
+            i /= 10;
+        }
+    }
+}else
+{
+    Console.WriteLine("El valor "+s+" no se puede transformar a entero. ");
 }
-
-*/
-
-
-//-------------------------------------- Punto 2 --------------------------------------
-
-int opcion=0;
+//Ejercicio 2
+int op = 0;
 do
 {
+    Console.WriteLine("Ingrese un Numero Segun la operacion deseada:");
+    Console.WriteLine("1. Sumar");
+    Console.WriteLine("2. Restar");
+    Console.WriteLine("3. Multiplicar");
+    Console.WriteLine("4. Dividir");
+    Console.WriteLine("5. Valor Absoluto");
+    Console.WriteLine("6. Cuadrado");
+    Console.WriteLine("7. Raíz Cuadrada");
+    Console.WriteLine("8. Seno");
+    Console.WriteLine("9. Coseno");
+    Console.WriteLine("10. Parte Entera");
+    Console.WriteLine("11. Numero Maximo");
+    Console.WriteLine("12. Numero Minimo");
+    Console.WriteLine("0. Salir");
 
-Console.WriteLine("-------------CALCULADORA-------------:");
-Console.WriteLine("Ingrese una opcion:");
-Console.WriteLine("1: Sumar");
-Console.WriteLine("2: Restar");
-Console.WriteLine("3: Multiplicar");
-Console.WriteLine("4: Dividir");
-Console.WriteLine("5: Salir");
+    string? sop = Console.ReadLine();
+    bool resultop = int.TryParse(sop, out op); //op=numerico
 
+    if (resultop)
+    {
+        if (op == 0)
+        {
+            Console.WriteLine("Saliendo...");
+        }
+        else
+        {
+            if (op < 5 || op==11|| op ==12)
+            {
+                Console.WriteLine("Ingrese una Numero a para operar:");
+                int a = 0;
+                string? s1 = Console.ReadLine();
+                bool result1 = int.TryParse(s1, out a); //a=numerico
 
-opcion=int.Parse(Console.ReadLine());
+                int b = 0;
+                Console.WriteLine("Ingrese un Numero b para operar:");
+                string? s2 = Console.ReadLine();
+                bool resul2 = int.TryParse(s2, out b); //b=numerico
 
-if(opcion<0 || opcion>5){
+                int r = 0;
+                if (resul2 && result1)
+                {
+                    string ms1="";
+                    switch (op)
+                    {
+                        case 1:
+                            r = a + b;
+                            ms1 = "La suma de  " +a+ " con " +b;
+                            break;
+                        case 2:
+                            r = a - b;
+                            ms1 = "La resta de  " +a+ " con " +b;
+                            break;
+                        case 3:
+                            r = a * b;
+                            ms1 = "El producto de  " +a+ " con " +b;
+                            break;
+                        case 4:
+                            r = a / b;
+                            ms1 = "El cociente de  " +a+ " con " +b;
+                            break;
+                        case 11:
+                            if (a > b)
+                            {
+                                Console.WriteLine("EL numero " + a + " es mayor que el numero " + b);
+                            }
+                            else
+                            {
+                                Console.WriteLine("EL numero " + b + " es mayor que el numero " + a);
+                            }
+                            break;
+                        case 12:
+                            if (b < a)
+                            {
+                                Console.WriteLine("EL numero " + a + " es menor que el numero " + b);
+                            }
+                            else
+                            {
+                                Console.WriteLine("EL numero " + b + " es menor que el numero " + a);
+                            }
+                            break;
+                    }
+                    Console.WriteLine(ms1+" es igual a  " + r);
+                }
+                else
+                {
+                    Console.WriteLine("valor ingresado de a o b es invalido.");
+                    op = 0;
+                }
+
+            }
+            else
+            {
+                if (op < 11 && op > 4)
+                {
+                    Console.WriteLine("Ingrese un Numero pera operar:");
+                    float c = 0;
+                    string? s3 = Console.ReadLine();
+                    bool result3 = float.TryParse(s3, out c); //c=numerico
+                    if (result3)
+                    {
+                        float r = 0;
+                        string ms="";
+                        switch (op)
+                        {
+                            case 5:
+                                r = Math.Abs(c);
+                                ms = "El valor absoluto de " + c;
+                                break;
+                            case 6:
+                                r = c * c;
+                                ms = "El cuadrado de " + c;
+                                break;
+                            case 7:
+                                r = (float)Math.Sqrt(c);
+                                ms = "La raiz Cuadrada de " + c;
+                                break;
+                            case 8:
+                                r = (float)Math.Sin(c);
+                                ms = "El Seno de " + c;
+                                break;
+                            case 9:
+                                r = (float)Math.Cos(c);
+                                ms = "El Coseno de " + c;
+                                break;
+                            case 10:
+                                string[] parteEntera = s3.Split(".");
+                                float.TryParse(parteEntera[0], out r);//Parte entera de la cadena s3 ingresada
+                                break;
+                        }
+                        Console.WriteLine(ms+" es igual a "+ r);
+                    }
+                    else
+                    {
+                        Console.WriteLine("valor ingresado del numero es invalido.");
+                        op = 0;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("El valor de operacion no es compatible con ninguna operacion.");
+                }
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("valor ingresado para realizar una operacion es invalido.");
+        op = 0;
+    }
+    Console.WriteLine("Desea realizar otra Operacion?.");
+    Console.WriteLine("1. si");
+    Console.WriteLine("0. no");
+    string? p = Console.ReadLine();
+    bool resulp = int.TryParse(p, out op); //op=numerico
     
-Console.WriteLine("Opcion no valida");
-
-}
-else{
-Console.WriteLine("Ingrese el primer valor");
-int valor1=int.Parse(Console.ReadLine());
-Console.WriteLine("\nIngrese el segundo valor");
-int valor2=int.Parse(Console.ReadLine());
-
-int resultado=0;
-switch (opcion)
-{
-    case 1:
-        resultado=valor1+valor2;
-        Console.WriteLine("\n\n"+valor1+"+"+valor2+"="+resultado);
-    break;
-    case 2:
-        resultado=valor1-valor2;
-        Console.WriteLine("\n\n"+valor1+"-"+valor2+"="+resultado);
-    break;
-    case 3:
-        resultado=valor1*valor2;
-        Console.WriteLine("\n"+valor1+"*"+valor2+"="+resultado);
-    break;
-    case 4:
-        resultado=valor1/valor2;
-        Console.WriteLine("\n"+valor1+"/"+valor2+"="+resultado);
-    break;
-    case 5:
-        break;
-    
-    
-}
-Console.WriteLine("\n Desea realizar otro calculo?");
-Console.WriteLine("1:Si");
-Console.WriteLine("2:No");
-int continuar=int.Parse(Console.ReadLine());
-if (continuar==2){
-    Console.WriteLine("\nSaliendo");
-    opcion=5;
-}
-}
-} while (opcion!=5);
-
-
-
-
-
-
+    if (!resulp || op != 1)
+    {
+        if (op == 0)
+        {
+            Console.WriteLine("Saliendo...");
+        }else
+        {
+            Console.WriteLine("valor ingresado para realizar una operacion es invalido.");
+            op = 0;
+        }
+    }
+} while (op != 0);
